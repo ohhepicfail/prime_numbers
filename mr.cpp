24cd6bd90e91d17ca7680054b900ae224ce9c9d0
ohhepicfail <ohh.epicfail@gmail.com>
2016-09-23 15:12:41 +0300

    pow_mod () overflow test

--- mr.cpp
+++ mr.cpp
@@ -3,15 +3,14 @@
 
 int main ()
 {
-	llu_t number = 1000000000;
-
-	for (llu_t n = 0; n < number; n++)
+	for (llu_t number = 100; number < 10000000000000; number *= 10)
 	{
-		if (is_prime (NULL, n) != is_prime_mr (n))
-			printf ("\n---!!!---fail---!!!--- --%llu\t%d\t%d\n", n, is_prime (NULL, n), is_prime_mr (n));
-		if (n % 1000000 == 0)
-			printf ("-%llu-", n);
+		for (llu_t n = number; n < 100 + number; n++)
+			if (is_prime (NULL, n) != is_prime_mr (n))
+				printf ("\n---!!!---fail---!!!--- --%llu\t%d\t%d\n", n, is_prime (NULL, n), is_prime_mr (n));
+		printf (".");
 	}
+
 
 	return 0;
 }

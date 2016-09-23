76a1d574f9894258971809a63591de25f17ed912
ohhepicfail <ohh.epicfail@gmail.com>
2016-09-23 14:48:21 +0300

    write some tests

--- mr.cpp
+++ mr.cpp
@@ -5,10 +5,12 @@ int main ()
 {
 	llu_t number = 1000000000;
 
-	for (llu_t n = number; n < 100 + number; n++)
+	for (llu_t n = 0; n < number; n++)
 	{
 		if (is_prime (NULL, n) != is_prime_mr (n))
 			printf ("\n---!!!---fail---!!!--- --%llu\t%d\t%d\n", n, is_prime (NULL, n), is_prime_mr (n));
+		if (n % 1000000 == 0)
+			printf ("-%llu-", n);
 	}
 
 	return 0;
